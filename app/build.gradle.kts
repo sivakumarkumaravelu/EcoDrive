05800bbc46733bb6df99f37cfc84a030c093cba3
Siva Kumar <sivakumar.kumaravelu@gmail.com>
2026-06-10 01:08:46 -0400

    Release version 1.3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.ecodrive.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.3.6"
+        versionCode = 11
+        versionName = "1.3.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
