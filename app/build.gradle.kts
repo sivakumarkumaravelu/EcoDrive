5c11e8957b601a5131dd9bca519917cb0fb5bf98
Siva Kumar <sivakumar.kumaravelu@gmail.com>
2026-06-11 01:09:01 -0400

    Bump version to 1.3.11 and add release APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.ecodrive.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.3.10"
+        versionCode = 15
+        versionName = "1.3.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
