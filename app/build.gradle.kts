f2344008215feae1466397ecd65b9fea5b03dc6e
Siva Kumar <sivakumar.kumaravelu@gmail.com>
2026-06-28 00:27:35 -0400

    chore: release version 1.3.27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.ecodrive.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 30
-        versionName = "1.3.26"
+        versionCode = 31
+        versionName = "1.3.27"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
